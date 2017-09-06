2461e388a31299c6211879466f5551f9b54433b0
Dmitriy <dgrushetskiy@gmail.com>
2017-09-06 22:08:56 +0300

    update ItemsController add CRUD json

--- src/main/java/io/khasang/teamnote/controller/ItemsController.java
+++ src/main/java/io/khasang/teamnote/controller/ItemsController.java
@@ -1,9 +1,12 @@
 package io.khasang.teamnote.controller;
 
+import io.khasang.teamnote.entity.Items;
 import io.khasang.teamnote.service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author d.grushetskiy
@@ -18,4 +21,36 @@ public class ItemsController {
     public ItemsController(ItemsService itemsService) {
         this.itemsService = itemsService;
     }
+
+    @RequestMapping(value = "/add", method = RequestMethod.PUT, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public Items addItems(@RequestBody Items items){
+        return itemsService.addItems(items);
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Items> getItems(){
+        return itemsService.getList();
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Items getById(@PathVariable(value = "id")String inputId){
+        return itemsService.getById(Long.parseLong(inputId));
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public Items update(@RequestBody Items items){
+        return itemsService.update(items);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public Items delete(@PathVariable(value = "id") String inputId){
+        return itemsService.delete(Long.parseLong(inputId));
+    }
+
+
 }
